c440585350fd3bd4215666410c292db7f7d5378c
markoknezevic <you@example.com>
2019-04-07 23:01:44 +0200

    TransmittingData_and_Timers

--- IntentKotlin/app/src/main/java/com/example/intentkotlin/Main2Activity.kt
+++ IntentKotlin/app/src/main/java/com/example/intentkotlin/Main2Activity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main2.*
+
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val intent=intent
+        val recived: String=intent.getStringExtra("input")
+        textView2.text=recived
     }
+
     fun changeAct(view:View){
         val intent= Intent(applicationContext,MainActivity::class.java)
         startActivity(intent)
--- IntentKotlin/app/src/main/java/com/example/intentkotlin/MainActivity.kt
+++ IntentKotlin/app/src/main/java/com/example/intentkotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     }
     fun changeActivity(view:View){
         val intent=Intent(applicationContext,Main2Activity::class.java)
+        intent.putExtra("input",editText.text.toString())
         startActivity(intent)
 
     }
