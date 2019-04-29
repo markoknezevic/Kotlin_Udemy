cfb76cba91ab7e9a26bcff78c95572f127277dff
markoknezevic <you@example.com>
2019-04-29 23:20:19 +0200

    Travel_Book_END

--- Travel_Book/app/src/main/java/com/example/travel_book/MainActivity.kt
+++ Travel_Book/app/src/main/java/com/example/travel_book/MainActivity.kt
@@ -1,17 +1,143 @@
 package com.example.travel_book
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_main.*
+
+val nameArray = ArrayList<String>()
+val locationArray = ArrayList<LatLng>()
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        try {
+
+            val database = openOrCreateDatabase("Places", Context.MODE_PRIVATE, null)
+            database.execSQL("CREATE TABLE IF NOT EXISTS places(name VARCHAR,latitude VARCHAR,longitude VARCHAR)")
+            val cursor = database.rawQuery("SELECT * FROM places", null)
+
+            val nameIx = cursor.getColumnIndex("name")
+            val latitudeIx = cursor.getColumnIndex("latitude")
+            val longitudeIx = cursor.getColumnIndex("longitude")
+
+            cursor.moveToFirst()
+
+            nameArray.clear()
+            locationArray.clear()
+
+
+            while (cursor != null) {
+
+                val nameDb = cursor.getString(nameIx)
+                val latDb = cursor.getString(latitudeIx).toDouble()
+                val lonDb = cursor.getString(longitudeIx).toDouble()
+
+                nameArray.add(nameDb)
+
+                val location = LatLng(latDb,lonDb)
+
+                locationArray.add(location)
+
+                cursor.moveToNext()
+
+            }
+        println(nameArray)
+            cursor?.close()
+        }catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,nameArray)
+        listView.adapter = arrayAdapter
+
+
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, id ->
+            val intent = Intent(applicationContext, MapsActivity :: class.java)
+
+            intent.putExtra("address",nameArray[position])
+            intent.putExtra("lat",locationArray[position].latitude)
+            intent.putExtra("lon",locationArray[position].longitude)
+            intent.putExtra("info","old")
+            startActivity(intent)
+
+
+        }
     }
+
+
+
+    override fun onResume() {
+        try {
+
+            val database = openOrCreateDatabase("Places", Context.MODE_PRIVATE, null)
+            database.execSQL("CREATE TABLE IF NOT EXISTS places(name VARCHAR,latitude VARCHAR,longitude VARCHAR)")
+            val cursor = database.rawQuery("SELECT * FROM places", null)
+
+            val nameIx = cursor.getColumnIndex("name")
+            val latitudeIx = cursor.getColumnIndex("latitude")
+            val longitudeIx = cursor.getColumnIndex("longitude")
+
+            cursor.moveToFirst()
+
+            nameArray.clear()
+            locationArray.clear()
+
+
+            while (cursor != null) {
+
+                val nameDb = cursor.getString(nameIx)
+                val latDb = cursor.getString(latitudeIx).toDouble()
+                val lonDb = cursor.getString(longitudeIx).toDouble()
+
+                nameArray.add(nameDb)
+
+                val location = LatLng(latDb,lonDb)
+
+                locationArray.add(location)
+
+                cursor.moveToNext()
+
+            }
+            println(nameArray)
+            cursor?.close()
+        }catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,nameArray)
+        listView.adapter = arrayAdapter
+
+
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, id ->
+            val intent = Intent(applicationContext, MapsActivity :: class.java)
+
+            intent.putExtra("address",nameArray[position])
+            intent.putExtra("lat",locationArray[position].latitude)
+            intent.putExtra("lon",locationArray[position].longitude)
+            intent.putExtra("info","old")
+            startActivity(intent)
+
+
+        }
+
+        super.onResume()
+    }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val manuInflater=menuInflater
@@ -20,11 +146,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if(item?.itemId == R.id.map)
+        if(item!!.itemId == R.id.map)
         {
             val  intent = Intent(applicationContext,MapsActivity :: class.java)
+            intent.putExtra("info","new")
             startActivity(intent)
         }
         return super.onOptionsItemSelected(item)
     }
+
+
 }
+
--- Travel_Book/app/src/main/java/com/example/travel_book/MapsActivity.kt
+++ Travel_Book/app/src/main/java/com/example/travel_book/MapsActivity.kt
@@ -4,11 +4,14 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,7 +23,10 @@ import java.util.*
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    var locationManager : LocationManager ?= null
+    var locationManager : LocationManager?= null
+    var locationListener : LocationListener ?= null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -41,82 +47,117 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapLongClickListener(myListener)
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
+        locationListener = object  : LocationListener{
+            override fun onLocationChanged(location: Location?) {
 
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),1)
-
-        }else{
-            var geocoder = Geocoder(applicationContext, Locale.getDefault())
-
-            var address=""
-            try {
-
-
-                val location = locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                val userLocation = LatLng(location.latitude, location.longitude)
-                val addreslist = geocoder.getFromLocation(userLocation.latitude,userLocation.longitude,1)
-
-                if(addreslist.size > 0){
-
-                    if(addreslist[0].thoroughfare != null){
-                        address += addreslist[0].thoroughfare.toString()+" "
+                if(location != null){
+                    try {
+                        mMap.clear()
+                        val userLocation = LatLng(location.latitude, location.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,13f))
+                    }catch (e : Exception){
+                        e.printStackTrace()
                     }
-                    if(addreslist[0].subThoroughfare != null){
-                        address += addreslist[0].subThoroughfare
-                    }
+
 
                 }
-                if(address.equals(""))
-                    address = "No Adress"
+            }
 
-                mMap.addMarker(MarkerOptions().position(userLocation).title(address))
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15f))
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
 
-            }catch (e : Exception){
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+
+            }
+
+
+        }
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),1)
+
+        }else{
+           // locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER,2,2f,locationListener)
+            val intent = intent
+            val info =intent.getStringExtra("info")
+
+            if(info.equals("new")) {
+                val location = locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                val uloc = LatLng(location.latitude, location.longitude)
+                mMap.addMarker(MarkerOptions().position(uloc).title("Your Location"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uloc, 13f))
+
+            }else{
+                val address = intent.getStringExtra("address")
+                val lat = intent.getDoubleExtra("lat",0.0)
+                val lon = intent.getDoubleExtra("lon",0.0)
+
+                val location = LatLng(lat,lon)
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,13f))
+                mMap.addMarker(MarkerOptions().position(location).title(address))
 
             }
         }
+
 
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if(requestCode == 1 && grantResults.size > 0){
-            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                try {
+        if(grantResults.size > 0 && requestCode ==1){
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+               locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2, 2f, locationListener)
 
-                    val location = locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                    val userLocation = LatLng(location.latitude, location.longitude)
-                    var geocoder = Geocoder(applicationContext, Locale.getDefault())
-
-                    val addreslist = geocoder.getFromLocation(userLocation.latitude,userLocation.longitude,1)
-
-                    var address=""
-
-                    if(addreslist.size > 0){
-
-                        if(addreslist[0].thoroughfare != null){
-                            address += addreslist[0].thoroughfare.toString()+" "
-                        }
-                        if(addreslist[0].subThoroughfare != null){
-                            address += addreslist[0].subThoroughfare
-                        }
-
-                    }
-
-                    if(address.equals(""))
-                        address = "No Adress"
-
-                    mMap.addMarker(MarkerOptions().position(userLocation).title(address))
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
-
-                }catch (e : java.lang.Exception){
-
-                }
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+    var myListener = object : GoogleMap.OnMapLongClickListener {
+        override fun onMapLongClick(p0: LatLng?) {
+            mMap.clear()
+            val geocoder = Geocoder(applicationContext, Locale.getDefault())
+            var address = ""
+            val addresslist=geocoder.getFromLocation(p0!!.latitude,p0.longitude,1)
+            if(addresslist.size > 0){
+
+                if(addresslist[0].thoroughfare != null)
+                    address += addresslist[0].thoroughfare
+                if(addresslist[0].subThoroughfare != null)
+                    address += addresslist[0].subThoroughfare
+
+            }else{
+                address = "New Place"
+            }
+            mMap.addMarker(MarkerOptions().position(p0).title(address))
+            Toast.makeText(applicationContext,"New Place Created",Toast.LENGTH_LONG).show()
+            try {
+                val latitude = p0.latitude.toString()
+                val longitude = p0.longitude.toString()
+                val database = openOrCreateDatabase("Places",Context.MODE_PRIVATE,null)
+
+                database.execSQL("CREATE TABLE IF NOT EXISTS places(name VARCHAR,latitude VARCHAR,longitude VARCHAR)")
+                val sqlString="INSERT INTO places(name,latitude,longitude) VALUES(?,?,?)"
+                val statment = database.compileStatement(sqlString)
+                statment.bindString(1,address)
+                statment.bindString(2,latitude)
+                statment.bindString(3,longitude)
+
+                statment.execute()
+
+            }catch (e : Exception){
+                e.printStackTrace()
+            }
+        }
+
     }
 }
